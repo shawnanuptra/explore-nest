6672dc6621d940c6ad879fc3794cbbe184bee624
Shawn Martin <shawn.martin@thenbs.com>
2026-01-06 11:39:20 +0000

    add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // where the automated schema file is generated. set to (true) to store in memory
+      sortSchema: true, // sorts schema based on lexicon (alphabetically). defaults to order of modules included
+      graphiql: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
